c40e0b627686b02ef640dfca8882d717602be0fa
Quinn Hanam <qhanam@uwaterloo.ca>
2014-09-17 23:30:07 -0400

    Write functions now 1.6 compatable and synchronous.

--- src/ca/uwaterloo/ece/qhanam/jrsrepair/Utilities.java
+++ src/ca/uwaterloo/ece/qhanam/jrsrepair/Utilities.java
@@ -3,9 +3,9 @@ package ca.uwaterloo.ece.qhanam.jrsrepair;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.nio.file.StandardOpenOption;
 
 public class Utilities {
 	
@@ -21,17 +21,17 @@ public class Utilities {
 //                    StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
 //	}
 
-	/**
-	 * Write function for JVM >= 1.7
-	 * @param file
-	 * @param content
-	 * @throws Exception
-	 */
-	public static void writeToFileAppend(File file, byte[] content) throws Exception{
-        Files.write(Paths.get(file.getPath()), content, 
-                    StandardOpenOption.SYNC, StandardOpenOption.WRITE, 
-                    StandardOpenOption.APPEND, StandardOpenOption.CREATE);
-	}
+//	/**
+//	 * Write function for JVM >= 1.7
+//	 * @param file
+//	 * @param content
+//	 * @throws Exception
+//	 */
+//	public static void writeToFileAppend(File file, byte[] content) throws Exception{
+//        Files.write(Paths.get(file.getPath()), content, 
+//                    StandardOpenOption.SYNC, StandardOpenOption.WRITE, 
+//                    StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+//	}
 
 	/**
 	 * Write function for JVM <= 1.6
@@ -50,29 +50,47 @@ public class Utilities {
 		fos.getFD().sync();
 		fos.close();
 	}
-	
+
 	/**
-	 * Read function for JVM >= 1.7
+	 * Write function for JVM <= 1.6
 	 * @param file
-	 * @return
+	 * @param content
 	 * @throws Exception
 	 */
-	public static byte[] readFromFile(File file) throws Exception{
-        byte[] bytes = Files.readAllBytes(Paths.get(file.getPath()));
-        return bytes;
+	public static void writeToFileAppend(File file, byte[] content) throws Exception{
+		FileOutputStream fos = new FileOutputStream(file, true);
+		
+		/* We need to synchronize the file on all devices before we
+		 * move on. If the file system is not synchronized, JUnit might
+		 * use old .class files and give incorrect results. */
+		fos.write(content);
+		fos.flush();
+		fos.getFD().sync();
+		fos.close();
 	}
-
+	
 //	/**
-//	 * Read function for JVM <= 1.6
+//	 * Read function for JVM >= 1.7
 //	 * @param file
 //	 * @return
 //	 * @throws Exception
 //	 */
 //	public static byte[] readFromFile(File file) throws Exception{
-//		FileInputStream fis = new FileInputStream(file);
-//		byte[] bytes = new byte[fis.available()];
-//		fis.read(bytes);
-//		fis.close();
-//		return bytes;
+//        byte[] bytes = Files.readAllBytes(Paths.get(file.getPath()));
+//        return bytes;
 //	}
+
+	/**
+	 * Read function for JVM <= 1.6
+	 * @param file
+	 * @return
+	 * @throws Exception
+	 */
+	public static byte[] readFromFile(File file) throws Exception{
+		FileInputStream fis = new FileInputStream(file);
+		byte[] bytes = new byte[fis.available()];
+		fis.read(bytes);
+		fis.close();
+		return bytes;
+	}
 }
